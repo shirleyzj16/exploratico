d5c3a10ce4fbd0fb3f027706906201d3fdecca38
shirleyzj16 <70995515+shirleyzj16@users.noreply.github.com>
2022-04-23 16:22:24 -0600

    first commit

---
+++ wp-content/themes/aplicaciones2022/index.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WORDPRESS</title>
+    
+</head>
+
+<body>
+   <!-- container -->
+    <div>
+        <!-- header -->
+        <header>
+        <div>
+                <h1>EXPLORATICO</h1>
+            </div>
+            <!-- nav -->
+            <nav>
+            
+                <ul>
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="#">COMIDA</a></li>
+                    <li><a href="#">ACTIVIDADES AIRE LIBRE</a></li>
+                    <li><a href="#">ARTE Y ARQUITECTURA</a></li>
+                    <li><a href="#">SOBRE NOSOTROS</a></li>
+                </ul>
+            </nav>
+            <!-- nav -->
+            
+        </header>
+        <!-- header -->
+
+        <!-- content -->
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab molestiae repellendus illum consequatur atque cumque ea eos voluptates minus ipsum! Consequuntur quam esse fuga eius nostrum ullam in quaerat provident!</p>
+        <!-- content -->  
+
+        <!-- footer -->
+        <footer>
+            <div>
+                <nav>
+                    <ul>
+                        <li><a href="#">YOUTUBE</a></li>
+                        <li><a href="#">FACEBOOK</a></li>
+                        <li><a href="#">CORREO</a></li>
+                        <li><a href="#">INSTRAGRAM</a></li>
+                    </ul>
+                </nav>
+                <p>2022 &copy;</p>
+            </div>
+        </footer>
+        <!-- footer -->
+
+    </div>
+    <!-- container -->
+</body>
+
+</html>
