f709922414f57575596291a07c86b5ba7a845f4b
shirleyzj16 <70995515+shirleyzj16@users.noreply.github.com>
2022-05-10 14:02:59 -0600

    new

--- wp-content/themes/aplicaciones2022/page-arte-y-arquitectura.php
+++ wp-content/themes/aplicaciones2022/page-arte-y-arquitectura.php
@@ -2,6 +2,25 @@
 
 <h1>Arte y arquitectura</h1>
 
+<div  class="container-fluid h-75">
+    <div >
+        <img src="images/fototeatro.jpg" alt="">
+    </div>    
+    <div class="info_lugar">
+        <h2>Nombre lugar</h1>
+        <h3>Provincia</h3>
+                <p>
+                El Teatro Nacional de Costa Rica es el principal teatro de Costa Rica.Se le considera uno de los inmuebles más 
+                importantes de la historia nacional, y principal joya arquitectónica de la ciudad de San José. Su construcción 
+                guarda un profundo significado dado que representa la decisión del costarricense a la hora de emprender acción y 
+                de la estabilidad económica (traída por la cosecha de café) y política de la época en la que fue construido.
+                 
+               </p>
+    </div>
+
+
+</div>
+
 
 
 <?php get_footer(); ?> 
